fda0641db7387252518e2da710f8a535d7dfff7a
DanielCampos214 <daniel.s.s.campos@gmail.com>
2022-11-22 02:32:34 -0300

    firstCommit

---
+++ questions.js
@@ -0,0 +1,86 @@
+export default [{
+    question: "Quais desses nomes não eram um casal?",
+    answers: [
+    { option: "Abraão e Sara", correct: false },
+    { option: "Jacó e Ana", correct: true },
+    { option: "Isaque e Rebeca", correct: false },
+    { option: "Isacc e Raquel", correct: false },]
+},
+{
+    question: "Qual o nome e a idade da pessoa mais velha mencionada na Bíblia?",
+
+    answers: [
+        { option: "Enos, viveu 905 anos", correct: false },
+        { option: "Noé, viveu 990 anos", correct: false },
+        { option: "Matusalém, viveu 969 anos", correct: true },
+        { option: "Rainha Ester, viveu 859 anos", correct: false },]
+
+}, {
+    question: "Qual desses não teve o seu nome mudado na Bíblia?",
+
+    answers: [
+        { option: "Sara", correct: false },
+        { option: "Abraão", correct: false },
+        { option: "Jacó", correct: false },
+        { option: "Davi", correct: true },]
+}, {
+    question: "Qual dos nomes de Deus Moisés deveria dar aos israelitas, quando falasse de quem tinha lhe enviado?",
+
+    answers: [
+        { option: "Elohim", correct: false },
+        { option: "El Shadday", correct: false },
+        { option: "Eu sou o que sou", correct: true },
+        { option: "Eu sou o Senhor", correct: false },]
+
+}, {
+    question: "Que animal falou com Balaão?",
+    answers: [
+        { option: "jumenta", correct: true },
+        { option: "camelo", correct: false },
+        { option: "cordeiro", correct: false },
+        { option: "pomba", correct: false },]
+}, {
+    question: "Quando bebê, como Moisés foi salvo do decreto infanticida do Faraó?",
+
+    answers: [
+        { option: "Foi levado às pressas para fora do Egito", correct: false },
+        { option: "Foi escondido dentro de uma caverna", correct: false },
+        { option: "Foi colocado num cesto e lançado no rio", correct: true },
+        { option: "Foi levado ao templo para servir a Deus", correct: false },]
+
+}, {
+    question: "Quantos eram os discípulos mais próximos de Jesus?",
+
+    answers: [
+        { option: 10, correct: false },
+        { option: 7, correct: false },
+        { option: 5, correct: false },
+        { option: 12, correct: true },]
+
+}, {
+    question: "Qual o nome da ilha onde João escreveu o livro de Apocalipse?",
+
+    answers: [
+        { option: "Ilha de Creta", correct: false },
+        { option: "Ilha de Malta", correct: false },
+        { option: "Ilha de Patmos", correct: true },
+        { option: "Ilha de Pérgamo", correct: false },]
+
+}, {
+    question: "Em quais livros da Bíblia é narrada a história do nascimento de Jesus?",
+
+    answers: [
+        { option: "Mateus e Lucas", correct: true },
+        { option: "Mateus e Marcos", correct: false },
+        { option: "Hebreus e Apocalipse", correct: false },
+        { option: "Gênesis e Salmos", correct: false },]
+}, {
+
+    question: "Quantos livros têm a bíblia evangélica?",
+    answers: [
+        { option: 46, correct: false },
+        { option: 76, correct: false },
+        { option: 101, correct: false },
+        { option: 66, correct: true },]
+},  
+];
